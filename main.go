92761fc3a5588fba28ae220b40df196a358ea2ea
Darya Kamynina <darya.kamynina@iqoption.com>
2021-12-27 03:04:18 +0300

    fix debug

--- main.go
+++ main.go
@@ -106,7 +106,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	setupLog.Info("starting manager!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
